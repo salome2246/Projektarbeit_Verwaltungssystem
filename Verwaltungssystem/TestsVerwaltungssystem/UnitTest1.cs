99143c700a9e6bd574f5735fa703aec759824663
sgi059130 <sgi059130@stud.gibb.ch>
2026-02-02 19:54:55 +0100

    test klasse erstellt

---
+++ Verwaltungssystem/TestsVerwaltungssystem/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Verwaltungssystem;
+
+namespace TestsVerwaltungssystem;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+        Benutzer benutzer1 = new Benutzer(1,"salome",Rolle.Projektleiter);
+        
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
