fd7747743d7692ee12e372f8f6c6255b1b053cd2
sgi059130 <sgi059130@stud.gibb.ch>
2026-03-03 08:25:06 +0100

    neue UnitTest Nunit4

--- Verwaltungssystem/Verwaltungssystem/obj/Debug/net9.0/Verwaltungssystem.AssemblyInfo.cs
+++ Verwaltungssystem/Verwaltungssystem/obj/Debug/net9.0/Verwaltungssystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Verwaltungssystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53ae09c4eb4b5eba1194a623b9b6cc1e39653923")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2150392d8e7258ece273fd6bafb52ec26490da5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Verwaltungssystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Verwaltungssystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
